afb91d9cd343deca862f6638537840b7e1439590
Matt Stratton <matt.stratton@gmail.com>
2015-07-24 15:26:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# picnic-basket
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'picnic-basket'
+maintainer 'Chef Software, Inc'
+maintainer_email 'mattstratton@chef.io'
+license 'apache2'
+description 'Installs/Configures picnic-basket'
+long_description 'Installs/Configures picnic-basket'
+version '0.1.0'
