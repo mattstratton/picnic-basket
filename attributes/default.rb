dab478753313a405ccec195611bb1129aff25a23
Matt Stratton <matt.stratton@gmail.com>
2015-07-24 18:14:24 -0500

    Override attributes for vagrant versions
    Fixes #3

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,6 @@
-default['vagrant']['version'] = '1.7.4'
+override['vagrant']['version'] = '1.7.4'
+override['vagrant']['url']         = vagrant_package_uri(node['vagrant']['version'])
+override['vagrant']['checksum']    = vagrant_sha256sum(node['vagrant']['version'])
 # case node['platform_family']
 # when 'windows'
 #   default['vagrant']['url'] = 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4.msi'
